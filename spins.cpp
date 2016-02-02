7df517dd3a2c4deefd91ad38e36af10bbd708199
Giacomo Torlai <torlaigiacomo@gmail.com>
2016-02-02 13:01:52 -0500

    Ising spins utilities

---
+++ spins.cpp
@@ -0,0 +1,68 @@
+#include "spins.h"
+#include "MersenneTwister.h"
+
+//Constructor 1
+Spins::Spins(){
+
+    spin.clear(); 
+
+}
+
+//Constructor 2
+Spins::Spins(int N_){
+
+    N = N_;
+
+    spin.resize(N,1); //assign every spin as 1
+
+}
+
+//Resize system and set spins to 1
+void Spins::resize(int N_){
+
+    N = N_;
+
+    spin.resize(N,1); //assign every spin as 1
+
+}
+
+//Randomize the spin values
+void Spins::randomize(MTRand & random){
+
+    int ising_spin;
+    
+    for (int i = 0; i<spin.size(); i++){
+        ising_spin = 2*random.randInt(1)-1;
+
+        spin.at(i) = ising_spin;
+    }//i
+
+
+}//randomize
+
+//Perform a single-spin flip
+void Spins::flip(int index){
+
+    spin.at(index) *= -1;
+
+}//flip
+
+
+//Print function
+void Spins::print(){
+
+    for (int i=0;i<spin.size();i++){
+        cout<<(spin[i]+1)/2<<" ";
+    }//i
+    cout<<endl;
+
+}//print
+
+//Print configuration on file
+void Spins::filePrint(ofstream & file){
+
+   for (int i=0;i<spin.size();i++){
+        file<<(spin[i]+1)/2<<" ";
+    }//i
+    file<<endl; 
+}
---
+++ spins.h
@@ -0,0 +1,32 @@
+#ifndef SPINS_H
+#define SPINS_H
+
+// spins.h
+// Class describing Ising spins on a lattice
+
+#include <vector>
+#include "MersenneTwister.h"
+#include <fstream>
+
+using namespace std;
+
+class Spins {
+    
+    public:
+    
+        int N;  //Number of spins
+
+        //Vector containing the spins values
+        vector<int> spin;
+
+        //Functions
+        Spins(int N_);
+        Spins();
+        void resize(int N_);
+        void flip(int index);
+        void print();
+        void randomize(MTRand & random);
+        void filePrint(ofstream & file); 
+};
+
+#endif
